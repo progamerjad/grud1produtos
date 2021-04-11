88845065d34eb063898b0514865b03fb31ff843a
Jadilson Domingos <jadilsondomingos86@gmail.com>
2021-04-11 20:19:36 -0300

    sibindo2

---
+++ src/views/produtos/cadastro.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class CadastroProduto extends React.Component{
+
+   render(){
+       return(
+           <div className="card">
+               <div className="card-header">
+                   Cadastro de produto
+               </div>
+               <div className="card-body">
+
+
+               </div>
+           </div>
+       )
+   }
+
+
+}
